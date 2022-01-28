ad2e578dd3ca3b56c4c235376d5b49a52e46f217
Windigo The Green <windigotp@gmail.com>
2022-01-28 18:58:48 +0800

    CameraState implemented

--- Rescues/Assets/Scripts/Controllers/Player/CameraController.cs
+++ Rescues/Assets/Scripts/Controllers/Player/CameraController.cs
@@ -7,19 +7,14 @@ namespace Rescues
     {
         #region Fields
 
-        private const float ACCELERATION_COEFFICIENT = 0.1f;
-
         private readonly GameContext _context;
         private readonly CameraServices _cameraServices;
         private CameraData _activeCamera;
-        private float _deadZone;
-        private float _cameraAccelerateStep;
-        private float _cameraAcceleration;
-        private float _targetPositionX;
-        private float _targetPositionY;
-        private bool _isMoveableCameraMode;
         private int _activeLocationHash;
-        private bool _isFocusedCameraMode;
+
+        private CameraState _currentState;
+        private CameraState _staticCameratState;
+        private CameraState _movableCameraState;
 
         #endregion
 
@@ -30,6 +25,9 @@ namespace Rescues
         {
             _context = context;
             _cameraServices = services.CameraServices;
+
+            _staticCameratState = new StaticCameraState(context, services);
+            _movableCameraState = new MovableCameraState(context, services);
         }
 
         #endregion
@@ -44,30 +42,8 @@ namespace Rescues
                 SetCamera();
             }
 
-            if (_cameraServices.IsCameraFocused && !_isFocusedCameraMode)
-            {
-                _isFocusedCameraMode = true;
-            }
-
-            if (!_cameraServices.IsCameraFocused && _isFocusedCameraMode)
-            {
-                _isFocusedCameraMode = false;
-            }
-
-            if (_isFocusedCameraMode && _context.character.IsMoving)
-            {
-                _cameraServices.ResetFocus();
-            }
-
-            if (_isMoveableCameraMode && !_isFocusedCameraMode)
-            {
-                MoveCameraToCharacter();
-            }
-
-            if (_isFocusedCameraMode)
-            {
-                MoveCameraToTarget();
-            }
+            if (_currentState != null)
+                _currentState.UpdateState();
 
             if (_cameraServices.IsCameraFree)
             {
@@ -118,7 +94,6 @@ namespace Rescues
 
         private void SetCamera()
         {
-            _isMoveableCameraMode = false;
             _activeCamera = _context.activeLocation.CameraData;
 
             switch (_activeCamera.CameraMode)
@@ -142,71 +117,17 @@ namespace Rescues
 
         private void PlaceCameraOnLocation()
         {
-            var position = _context.activeLocation.LocationInstance.CameraPosition;
-            _cameraServices.CameraMain.transform.position = new Vector3(position.x, position.y,
-                _cameraServices.CameraDepthConst);
+            _currentState = _staticCameratState;
+            _currentState.InitState(_activeCamera);
         }
 
         private void PresetMovableCamera()
         {
-            _cameraAccelerateStep = _activeCamera.CameraAccelerateStep * ACCELERATION_COEFFICIENT;
-            _cameraAcceleration = 1f;
-            _deadZone = _activeCamera.DeadZone;
-
-            _targetPositionX = _context.character.Transform.position.x + _activeCamera.Position_X_Offset;
-            var x = Mathf.Clamp(_targetPositionX, _activeCamera.MoveLeftXLimit, _activeCamera.MoveRightXLimit);
-            _cameraServices.CameraMain.transform.position = new Vector3(x, _activeCamera.Position_Y_Offset,
-                _cameraServices.CameraDepthConst);
-
-            _isMoveableCameraMode = true;
+            _currentState = _movableCameraState;
+            _currentState.InitState(_activeCamera);
         }
 
-        private void MoveCameraToCharacter()
-        {
-            _targetPositionX = _context.character.Transform.position.x + _activeCamera.Position_X_Offset;
-            _targetPositionY = _activeCamera.Position_Y_Offset;
-
-            if (_context.character.IsMoving == false)
-            {
-                _deadZone = _activeCamera.DeadZone;
-            }
-
-            MoveCamera();
-        }
-
-        
-
-        private void MoveCameraToTarget()
-        {
-            _targetPositionX = _cameraServices.OverridePosition.x + _activeCamera.Position_X_Offset;
-            _targetPositionY = _cameraServices.OverridePosition.y;
-
-            MoveCamera();
-        }
-
-        private void MoveCamera()
-        {
-            var cameraPositionX = _cameraServices.CameraMain.transform.position.x;
-            var cameraPositionY = _cameraServices.CameraMain.transform.position.y;
-
-            if (Mathf.Abs(_cameraServices.CameraMain.transform.position.x - _targetPositionX) > _deadZone ||
-                !Mathf.Approximately(cameraPositionY, _activeCamera.Position_Y_Offset))
-            {
-                _cameraAcceleration = _cameraAccelerateStep * Time.deltaTime;
-                _deadZone = 0;
-                _cameraAcceleration = Mathf.Clamp(_cameraAcceleration, 0f, 1f);
-                cameraPositionX = Mathf.Lerp(cameraPositionX, _targetPositionX, _cameraAcceleration);
-                cameraPositionY = Mathf.Lerp(cameraPositionY, _targetPositionY, _cameraAcceleration);
-            }
-
-            cameraPositionX = Mathf.Clamp(cameraPositionX, _activeCamera.MoveLeftXLimit, _activeCamera.MoveRightXLimit);
-            cameraPositionY = Mathf.Clamp(cameraPositionY, _activeCamera.MoveDownYLimit, _activeCamera.MoveUpYLimit);
-
-            _cameraServices.CameraMain.transform.position = new Vector3(cameraPositionX, cameraPositionY,
-                _cameraServices.CameraDepthConst);
-        }
-
-        private bool OnFilterHandler(Collider2D obj)
+         private bool OnFilterHandler(Collider2D obj)
         {
             return obj.CompareTag(TagManager.PLAYER);
         }
